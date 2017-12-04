e0afa8dd929cf64c24a9ffa11e623c4682732d5b
Ali1 <ali.m.bakir@gmail.com>
2017-12-04 23:05:31 +0000

    Create config-common-exchanges.js

---
+++ config-common-exchanges.js
@@ -0,0 +1,13 @@
+var exchanges = {};
+
+exchanges = {
+  poloniex: {
+    name: 'poloniex', // the real gekko name
+    passphrase: '', // GDAX, requires a passphrase.
+    key: '***',
+    secret: '***',
+    username: '' // your username, only required for specific exchanges.
+  }
+};
+
+module.exports = exchanges;
